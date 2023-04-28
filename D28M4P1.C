1d69c0d9c25806d4bd29e9f01d1ecef0564d9528
codecracker0123 <131762670+codecracker0123@users.noreply.github.com>
2023-04-28 16:01:02 +0530

    Add files via upload

---
+++ D28M4P1.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+     clrscr();
+     int a=15,b=10;
+     if(a>b)
+     {
+	    printf("a is minimum");
+     }
+     else
+     {
+	    printf("b is minimum");
+     }
+	    getch();
+}
